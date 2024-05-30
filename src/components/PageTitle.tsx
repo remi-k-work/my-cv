5ae313be015b95f70293a088e1986f69dedb36c9
Remi <remi.k.work@gmail.com>
2024-05-30 20:13:29 +0200

    new portfolio project

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -15,17 +15,21 @@ export default function PageTitle() {
   const { pathname } = useLocation();
 
   // Get the current page title data depending on the pathname of the location
-  const { eyebrow, heading } = pageTitles[pathname];
+  const { eyebrow, heading, intro } = pageTitles[pathname];
 
   return (
-    <section className={styles["page-title"]}>
-      <p className={styles["page-title__eyebrow"]}>{eyebrow}</p>
-      <h1 className={styles["page-title__heading"]}>{heading}</h1>
-      <p className={styles["page-title__intro"]}>
-        {/* Show the avatar only on the homepage */}
-        {pathname === "/" && <img src={avatar} width={288} height={288} alt="Avatar" />}
-        <TypeWriterOutput />
-      </p>
-    </section>
+    <>
+      <p className="sr-only">{intro}</p>
+
+      <section className={styles["page-title"]}>
+        <p className={styles["page-title__eyebrow"]}>{eyebrow}</p>
+        <h1 className={styles["page-title__heading"]}>{heading}</h1>
+        <p className={styles["page-title__intro"]}>
+          {/* Show the avatar only on the homepage */}
+          {pathname === "/" && <img src={avatar} width={288} height={288} alt="Avatar" />}
+          <TypeWriterOutput />
+        </p>
+      </section>
+    </>
   );
 }
