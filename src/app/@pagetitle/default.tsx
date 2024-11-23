5adc6c6efd7b95c441551837da5d2531e82d760a
Remi <remi.k.work@gmail.com>
2024-11-23 21:30:09 +0100

    looking for error

--- src/app/@pagetitle/default.tsx
+++ src/app/@pagetitle/default.tsx
@@ -1,11 +1,6 @@
 // components
-import GlobalContextFetcher from "@/lib/GlobalContextFetcher";
 import PageTitle from "@/components/PageTitle";
 
 export default function Default() {
-  return (
-    <GlobalContextFetcher>
-      <PageTitle />
-    </GlobalContextFetcher>
-  );
+  return <PageTitle />;
 }
